6661b11dda7cd486451f53f3c46546dbce54b1a1
root <3290@holbertonschool.com>
2022-01-03 10:46:54 -0800

    fixpuppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,4 @@
 #Puppet install Pacakge
   exec { 'puppet-lint': #InstallPackagePuppet lint
-	command => '/bin/gem install puppet-lint -v 2.5.0'
+  command => '/bin/gem install puppet-lint -v 2.5.0'
     }
