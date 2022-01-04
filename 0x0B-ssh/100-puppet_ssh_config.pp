9206ce4db624d9ccd6e9ca93c8bf125594eb93e5
root <3290@holbertonschool.com>
2022-01-04 05:59:19 -0800

    fix100?

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,13 +3,15 @@ include stdlib
 
 #import identity file
 
-file_line { 'lare line identity':
+file_line { 'Declare line identity':
+  ensure  => present,
   path    => '/etc/ssh/ssh_config',
   line    => 'IdentityFile ~/.ssh/school',
   replace => true
 #Disable Auntentication in ssh_config
 }
 file_line { 'Turn off passwd auth':
+  ensure  => present,
   path    => '/etc/ssh/ssh_config',
   line    => 'PasswordAutentication no',
   replace => true,
