2b6f98d70238c49d34e54bf2a2f13c31c54f3912
root <3290@holbertonschool.com>
2022-01-31 13:32:32 -0800

    fix?

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,14 +7,17 @@ from sys import argv
 
 
 if __name__ == '__main__':
-    userInfo = requests.get(
-        'https://jsonplaceholder.typicode.com/users/' + argv[1]).json()
-    tasks = requests.get(
-        'https://jsonplaceholder.typicode.com/todos?userId=' + argv[1]).json()
-    completedTasks = [
-        task.get('title') for task in tasks if task.get('completed')]
-    print('Employee {} is done with tasks({}/{}):'
-          .format(userInfo.get('name'), len(completedTasks), len(tasks)))
+    def func_api():
+        userInfo = requests.get(
+            'https://jsonplaceholder.typicode.com/users/' + argv[1]).json()
+        tasks = requests.get(
+            'https://jsonplaceholder.typicode.com/todos?userId=' + argv[1]).json()
+        completedTasks = [
+            task.get('title') for task in tasks if task.get('completed')]
+        print('Employee {} is done with tasks({}/{}):'
+            .format(userInfo.get('name'), len(completedTasks), len(tasks)))
 
-    for task in completedTasks:
-        print('     {}'.format(task))
+        for task in completedTasks:
+            print('\t {}'.format(task))
+
+func_api()
