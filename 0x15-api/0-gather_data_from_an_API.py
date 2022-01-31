52c0cd11957f77abc732dcc35fcdd40e00878d22
root <3290@holbertonschool.com>
2022-01-31 11:51:02 -0800

    fix0?

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -14,7 +14,6 @@ if __name__ == "__main__":
     for tasks in task:
         if tasks.get("completed") is True:
             taskscompleted.append(tasks.get('title'))
-    print(
-        "Employee {} is done with tasks({}/{}):".format(
-            name.get('name'), len(taskscompleted), len(task)))
+    print("Employee {} is done with tasks({}/{}):".
+            format(name.get('name'), len(taskscompleted), len(task)))
     print('\n'.join('\t {}'.format(task) for task in taskscompleted))
