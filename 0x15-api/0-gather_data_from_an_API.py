4d832d9433001ca1a5d7818bcc3705d035334cbb
root <3290@holbertonschool.com>
2022-01-31 15:06:46 -0800

    prueba10000000

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Data In API """
+""" For an employee ID, returns information about the TODO list progress. """
 import json
 import requests
 import sys
@@ -8,8 +8,8 @@ import sys
 if __name__ == "__main__":
     response = requests.get("https://jsonplaceholder.typicode.com/users/" +
                             sys.argv[1])
-    dic = json.loads(response.text)
-    name = dic.get('name')
+    dicti = json.loads(response.text)
+    name = dicti.get('name')
     response = requests.get("https://jsonplaceholder.typicode.com/todos/" +
                             "?userId=" + sys.argv[1])
     todos = json.loads(response.text)
@@ -18,4 +18,4 @@ if __name__ == "__main__":
     done = len(completed)
     print("Employee {} is done with tasks({}/{}):".format(name, done, tasks))
     for task in completed:
-        print("    ", task.get('title'))
+        print("\t", task.get('title'))
