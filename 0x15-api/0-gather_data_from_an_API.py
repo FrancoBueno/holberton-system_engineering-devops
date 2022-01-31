fcaf525676879cc53e6fe31f8e6e564f88b377b6
root <3290@holbertonschool.com>
2022-01-31 14:49:23 -0800

    fix?

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,10 +7,12 @@ import json
 
 
 if __name__ == "__main__":
-    resp = req.get('https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
+    resp = req.get('https://jsonplaceholder.typicode.com/users/{}'
+                   .format(argv[1]))
     dic = json.loads(resp.text)
     name = dic.get('name')
-    resp = req.get('https://jsonplaceholder.typicode.com/todos/?userId={}'.format(argv[1]))
+    resp = req.get('https://jsonplaceholder.typicode.com/todos/?userId={}'
+                   .format(argv[1]))
     todo = json.loads(resp.text)
     tasks = len(todo)
     completed = [task for task in todo if task.get("completed")]
