3f073b26c1e339dd0ddfa3101d39e5029df2220c
root <3290@holbertonschool.com>
2022-01-31 15:40:21 -0800

    fix

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
-"""
-asdjasdkas
-"""
-
+""" Export data in the CSV format.. """
 import json
 import requests
-from sys import argv
+import sys
 
-if __name__ == '__main__':
-    userId = argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                        format(userId), verify=False).json()
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
-                        format(userId), verify=False).json()
-    username = user.get('username')
-    tasks = []
-    for task in todo:
-        task_dict = {}
-        task_dict["task"] = task.get('title')
-        task_dict["completed"] = task.get('completed')
-        task_dict["username"] = username
-        tasks.append(task_dict)
-    jsonobj = {}
-    jsonobj[userId] = tasks
-    with open("{}.json".format(userId), 'w') as jsonfile:
-        json.dump(jsonobj, jsonfile)
+
+if __name__ == "__main__":
+    response = requests.get("https://jsonplaceholder.typicode.com/users/" +
+                            sys.argv[1])
+    dicti = json.loads(response.text)
+    username = dicti.get('username')
+    response = requests.get("https://jsonplaceholder.typicode.com/todos/" +
+                            "?userId=" + sys.argv[1])
+    todos = json.loads(response.text)
+    tasks = [task for task in todos]
+    ans = {sys.argv[1]: []}
+    for task in tasks:
+        task_dict = {"task": task.get('title'),
+                     'completed': task.get('completed'), 'username': username}
+        ans.get(sys.argv[1]).append(task_dict)
+    with open(sys.argv[1] + ".json", "w") as f:
+        json.dump(ans, f)
