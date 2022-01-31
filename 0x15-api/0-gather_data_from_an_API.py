22e78df337409e5362ee101b6353c50edc9b47a4
root <3290@holbertonschool.com>
2022-01-31 13:14:45 -0800

    fix0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,31 +2,18 @@
 """ API Project """
 
 
-if __name__ == "__main__":
-    import requests
-    from sys import argv
-    name = requests.get(
-        "https://jsonplaceholder.typicode.com/users").json()
-    task = requests.get(
-        "https://jsonplaceholder.typicode.com/todos").json()
-    user = {}
-    for usr in name:
-        if usr["id"] == int(argv[1]):
-            user = dict(usr)
-            break
-    taskcompleted = 0
-    user_all = []
-    user_comp = []
+import requests
+from sys import argv
 
-    for usr in task:
-        if usr["userId"] == int(argv[1]):
-            user_all.append(dict(usr))
-            if usr["completed"]:
-                taskcompleted = taskcompleted + 1
-                user_comp.append(dict(usr))
 
-    print("Employee {} is done with tasks({}/{}):"
-          .format(user["name"], taskcompleted, len(user_all)))
+if __name__ == '__main__':
+    userInfo = requests.get(
+        'https://jsonplaceholder.typicode.com/users/' + argv[1]).json()
+    tasks = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId=' + argv[1]).json()
+    completedTasks = [task.get('title') for task in tasks if task.get('completed')]
+    print('Employee {} is done with tasks({}/{}):'
+          .format(userInfo.get('name'), len(completedTasks), len(tasks)))
 
-    for tasks in user_comp:
-        print("\t {}".format(tasks["title"]))
+    for task in completedTasks:
+        print('\t {}'.format(task))
