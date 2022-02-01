54d67d65b4283dd534f686bee1713efcee1ad1b2
root <3290@holbertonschool.com>
2022-02-01 05:45:37 -0800

    fix3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -5,19 +5,24 @@ import csv
 import json
 import requests
 
-
 if __name__ == "__main__":
-    name = requests.get(
-        "https://jsonplaceholder.typicode.com/users/").json()
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users").json()
+    all = requests.get(url + "todos").json()
+    user_dict = {}
+    name_dict = {}
+    for field in user:
+        user_id = field.get("id")
+        user_dict[user_id] = []
+        name_dict[user_id] = field.get("username")
 
-    for users in name:
-        task = requests.get(
-            "https://jsonplaceholder.typicode.com/todos?userId=" + str(
-                users.get("id"))).json()
-    with open("todo_all_employees.json", mode='w') as File:
-        dictoftasks = [{"username": users.get("username"),
-                        "task": tasks.get("title"),
-                        "completed": tasks.get("completed")} for tasks in task]
-        dictnew = {}
-        dictnew.update({users.get("id"): dictoftasks})
-        json.dump(dictnew, File)
+    for task in all:
+        user_id = task.get("userId")
+        task_dic = {}
+        task_dic["task"] = task.get("title")
+        task_dic["completed"] = task.get("completed")
+        task_dic["username"] = name_dict.get(user_id)
+        user_dict.get(user_id).append(task_dic)
+
+    with open("todo_all_employees" + ".json", "w") as jsonFile:
+        json.dump(user_dict, jsonFile)
