d167bb8d455fec8963b889e168ada91116c2c955
root <3290@holbertonschool.com>
2022-02-15 10:00:43 -0800

    fix-pycode

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,10 +10,12 @@ def number_of_subscribers(subreddit):
     if subreddit is None or type(subreddit) is not str:
         return 0
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    header = {'User-Agent': 'linux:0x16.api.advanced:v1.0.0 (by u/Frankitooo019)'}
+    header = {
+        'User-Agent': 'linux:0x16.api.advanced:v1.0.0 (by u/Frankitooo019)'
+    }
     resp = requests.get(url, headers=header, allow_redirects=False)
     if resp.status_code == 404:
-        return 0 
+        return 0
     else:
         resultado = resp.json().get('data')
         return resultado.get('subscribers')
