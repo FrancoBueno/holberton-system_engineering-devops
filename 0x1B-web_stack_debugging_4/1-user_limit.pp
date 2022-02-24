edd73dbdc9d3b8194e85a8ba8619b2d8194a0d9c
root <3290@holbertonschool.com>
2022-02-24 09:02:01 -0800

    task-1(with --fix)

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,9 +1,9 @@
 # User limit
 exec { 'hard':
-    provider => 'shell',
+    provider => shell,
     command  => 'sed -i sed -i s/5/4069/ /etc/security/limits.conf'
 }
 exec { 'soft':
-    provider => 'shell',
-    command  =>'sed -i sed -i s/4/4069/ /etc/security/limits.conf'
+    provider => shell,
+    command  => 'sed -i sed -i s/4/4069/ /etc/security/limits.conf'
 }
