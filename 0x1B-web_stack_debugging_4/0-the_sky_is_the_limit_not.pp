8ab55ab211ba87c69b108be683122c970929906d
root <3290@holbertonschool.com>
2022-02-24 08:50:56 -0800

    fixpuppet

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 #increment the users nginx
 exec { 'change-limit':
-    provider => 'shell',
-    command => 'sed -i s/15/4069/ /etc/default/nginx; sudo service nginx restart'
+  provider => 'shell',
+  command  => 'sed -i s/15/4069/ /etc/default/nginx; sudo service nginx restart'
 }
